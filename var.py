3d30e36b95c036e86b15831f5e022680550621e1
Dzmitry_Dashkevich <Dzmitry_Dashkevich@epam.com>
2017-06-26 16:26:06 +0300

    variables

---
+++ var.py
@@ -0,0 +1,17 @@
+a = b = c = 8
+print a, b, c
+a, b, c = 1, 2, 3
+print a, b, c
+
+#initialize a variable
+var = 8
+print var
+#set a new decimal value
+var = 3.142
+print var
+#set a new string value
+var = 'Python steps'
+print var
+#set a new logic value
+var = True
+print var
