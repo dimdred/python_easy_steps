91e334a05c544742405c82bf7992c32809a1a420
Dzmitry_Dashkevich <Dzmitry_Dashkevich@epam.com>
2017-07-10 16:01:29 +0300

    module

--- modules.py
+++ modules.py
@@ -14,3 +14,25 @@ cat.purr(pet)
 cat.lick(pet)
 cat.nap(pet)
 
+# pooch
+from dog import bark, lick, nap
+# from dog import * # import all functions
+
+bark()
+lick()
+nap()
+
+bark('Pooch')
+lick('Pooch')
+nap('Pooch')
+
+# system
+import sys, keyword
+print 'Python version:', sys.version
+print 'Python Interpreter Location:', sys.executable
+print 'My module search path:'
+for dir in sys.path:
+    print dir
+print 'Python Keywords:'
+for word in keyword.kwlist:
+    print word
