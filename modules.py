6b8ac0378bf001375d2a8cbd61c7eb887204e616
Dzmitry_Dashkevich <Dzmitry_Dashkevich@epam.com>
2017-07-10 15:45:24 +0300

    module

---
+++ modules.py
@@ -0,0 +1,16 @@
+# kitty
+import cat
+
+cat.purr()
+cat.lick()
+cat.nap()
+
+cat.purr('Kitty')
+cat.lick('Kitty')
+cat.nap('Kitty')
+
+pet = input('Enter a pet Name:')
+cat.purr(pet)
+cat.lick(pet)
+cat.nap(pet)
+
